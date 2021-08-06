a9a77ca8bf50b641ac608e9512fe0771d2d43130
JoeRapso <joe.rapso@gmail.com>
2021-08-06 09:09:19 +0300

    final version

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:name>", views.entry, name="entry"),
-    path("search", views.search, name="search")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
+    path("createPage", views.createPage, name="createPage"),
+    path("createdPage", views.createdPage, name="createdPage"),
+    path("editPage", views.editPage, name="editPage"),
+    path("editedPage", views.editedPage, name="editedPage"),
+    path("randomPage", views.randomPage, name="randomPage")
 ]
-
-""" add an error status code render in path /string name route """
-
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,24 +1,82 @@
-import markdown2, re
-from django.shortcuts import render, HttpResponse
+import markdown2, random
+from django.shortcuts import render, HttpResponse, redirect
+from django.template.response import TemplateResponse
 from django import forms
 
 from . import util
 
+# Returns a list of entry titles and displays it in index.html
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
     })
-def entry(request, name):
+
+# Renders the contents of an entry file into HTML
+def entry(request, title):
     try:
         return render(request, "encyclopedia/entrypage.html", {
-            "content": markdown2.markdown(util.get_entry(name)),
-            "name": name.capitalize # test this further, test "ht", "cs" variations
+            "content": markdown2.markdown(util.get_entry(title)),
+            "title": title
         })
-    except: 
-        return HttpResponse('404 File not found', status=404)
+    except:
+        return render(request, "encyclopedia/error.html", {
+            "status": "404 error - file not found. Please try a different wiki entry."
+        }, status=404)
+
+# Searches for an entry based on the users query
 def search(request):
     if request.method == "POST":
         q = request.POST.get('q')
-        matches = [i for i in util.list_entries() if q[0].upper() in i] # test querry case sensitivity further, before final version
+        q = q.lower()
+        titles_list = util.list_entries()
+        titles_list = [x.lower() for x in titles_list]
+        if q in titles_list:
+            return redirect('entry', q)
+        matches = [i for i in util.list_entries() if q[0].upper() in i]
         return render(request, "encyclopedia/search.html", {"matching_list": matches})
+
+# Renders the newpage.html
+def createPage(request):
+
+    return render(request, "encyclopedia/newpage.html")
+
+# Grabs relevant variables from a form and creates a new wiki entry
+def createdPage(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        title_lower = title.lower()
+        titles_list = util.list_entries()
+        titles_list = [x.lower() for x in titles_list]
+        if title_lower in titles_list:
+            return render(request, "encyclopedia/error.html", {
+            "status": "403 forbidden error - file with same title already exists. Please type a different title."
+        }, status=403)
+        content = request.POST.get('content')
+        title= title[:1].upper() + title[1:]
+    util.save_entry(title, content)
+    return redirect('entry', title)
+
+# Grabs the content of an entry and displays it in the relevant HTML areas
+def editPage(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        return render(request, "encyclopedia/editpage.html", {"title": title, "content": util.get_entry(title)})
+
+# Allows for the contents of an entry to be changed/edited
+def editedPage(request):
+
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        util.save_entry(title, content)
+        return redirect('entry', title)
+
+# Grabs an entry at random and displays it
+def randomPage(request):
+    randomTitle = random.choice(util.list_entries())
+    return redirect('entry', randomTitle)
+
+
+
 
